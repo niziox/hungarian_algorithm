a29ec0d47b649fae7f8091dd789baf0029c70585
keliasz <KEliasz145@gmail.com>
2021-05-03 14:43:48 +0200

    Update hungarian_algorithm.py

--- hungarian_algorithm.py
+++ hungarian_algorithm.py
@@ -39,11 +39,14 @@ def search_zeros(G):
             for n, elem in enumerate(temp_G[minimum[1]]):
                 if elem == 0 and n not in taken_c and minimum[1] not in taken_r:
                     ind_zeros.append((minimum[1], n))
+                    break
+
             del rows_zeros[minimum[1]]
         else:
             for n, elem in enumerate(temp_G[:, minimum[1]]):
                 if elem == 0 and minimum[1] not in taken_c and n not in taken_r:
                     ind_zeros.append((n, minimum[1]))
+                    break
             del cols_zeros[minimum[1]]
         if rows_zeros:
             min_row = min(rows_zeros, key=rows_zeros.get)
@@ -56,6 +59,43 @@ def search_zeros(G):
     return ind_zeros
 
 
+def find_more_ind_zeros(G, ind_zeros):
+    num_of_lines = len(ind_zeros)
+    rows_zeros = {}
+    cols_zeros = {}
+    lines = []
+    for n, elem in enumerate(G):
+        rows_zeros[n] = np.count_nonzero(elem == 0)
+    for n, elem in enumerate(G):
+        cols_zeros[n] = np.count_nonzero(G[:, n] == 0)
+    while num_of_lines:
+        max_row = max(rows_zeros, key=rows_zeros.get)
+        max_col = max(cols_zeros, key=cols_zeros.get)
+        maximum = ('r', max_row) if rows_zeros[max_row] >= cols_zeros[max_col] else ('c', max_col)
+        if maximum[0] == 'r':
+            lines.append((maximum[0], max_row))
+            del rows_zeros[max_row]
+        else:
+            lines.append((maximum[0], max_col))
+            del cols_zeros[max_col]
+        num_of_lines -= 1
+    erased_rows = [i[1] for i in lines if i[0] == 'r']
+    erased_cols = [i[1] for i in lines if i[0] == 'c']
+    min_element = np.inf
+    for i, row in enumerate(G):
+        for j, element in enumerate(row):
+            if i not in erased_rows and j not in erased_cols:
+                if element < min_element:
+                    min_element = element
+    for i, row in enumerate(G):
+        for j, element in enumerate(row):
+            if i not in erased_rows and j not in erased_cols:
+                G[i][j] -= min_element
+            elif i in erased_rows and j in erased_cols:
+                G[i][j] += min_element
+    return G
+
+
 if __name__ == '__main__':
     m = np.array([[20, 40, 10, 50],
                   [100, 80, 30, 40],
@@ -63,3 +103,6 @@ if __name__ == '__main__':
                   [70, 30, 10, 25]])
 
     print(search_zeros(reduction(m)[0]))
+    s = find_more_ind_zeros(reduction(m)[0], search_zeros(reduction(m)[0]))
+    print(s)
+    print(search_zeros(s))
