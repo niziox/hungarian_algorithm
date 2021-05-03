bf5ea23968dd0a49e9168cb950683f2e4ec24175
keliasz <KEliasz145@gmail.com>
2021-05-03 13:20:39 +0200

    Update hungarian_algorithm.py

--- hungarian_algorithm.py
+++ hungarian_algorithm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import numpy as np
+
 
 def reduction(G):
     row = G.min(axis=1)
@@ -12,15 +12,48 @@ def reduction(G):
 
     return G, sum(row) + sum(col)
 
+
 def search_zeros(G):
-    row = [1, 2, 2, 1]
-    col = [1, 1, 3, 1]
-
-    for i in G:
-        for j in G[i]:
-            pass
-
-
+    rows_zeros = {}
+    cols_zeros = {}
+    ind_zeros = []
+    temp_G = G.copy()
+    for n, elem in enumerate(temp_G):
+        rows_zeros[n] = np.count_nonzero(elem == 0)
+    for n, elem in enumerate(temp_G):
+        cols_zeros[n] = np.count_nonzero(temp_G[:, n] == 0)
+    min_row = min(rows_zeros, key=rows_zeros.get)
+    min_col = min(cols_zeros, key=cols_zeros.get)
+    ozn_c = 0
+    ozn_r = 0
+    while rows_zeros or cols_zeros:
+        if ozn_c != 0:
+            minimum = ('r', min_row)
+        elif ozn_r != 0:
+            minimum = ('c', min_col)
+        else:
+            minimum = ('r', min_row) if rows_zeros[min_row] <= cols_zeros[min_col] else ('c', min_col)
+        taken_r = [i[0] for i in ind_zeros]
+        taken_c = [i[1] for i in ind_zeros]
+        if minimum[0] == 'r':
+            for n, elem in enumerate(temp_G[minimum[1]]):
+                if elem == 0 and n not in taken_c and minimum[1] not in taken_r:
+                    ind_zeros.append((minimum[1], n))
+            del rows_zeros[minimum[1]]
+        else:
+            for n, elem in enumerate(temp_G[:, minimum[1]]):
+                if elem == 0 and minimum[1] not in taken_c and n not in taken_r:
+                    ind_zeros.append((n, minimum[1]))
+            del cols_zeros[minimum[1]]
+        if rows_zeros:
+            min_row = min(rows_zeros, key=rows_zeros.get)
+        else:
+            ozn_r = np.inf
+        if cols_zeros:
+            min_col = min(cols_zeros, key=cols_zeros.get)
+        else:
+            ozn_c = np.inf
+    return ind_zeros
 
 
 if __name__ == '__main__':
@@ -29,5 +62,4 @@ if __name__ == '__main__':
                   [10, 5, 60, 20],
                   [70, 30, 10, 25]])
 
-    print(reduction(m))
-
+    print(search_zeros(reduction(m)[0]))
